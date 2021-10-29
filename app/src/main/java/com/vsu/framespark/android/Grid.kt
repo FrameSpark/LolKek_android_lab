e4c8edae449d872065285844918259ea38b465ca
framespark <432032@mail.ru>
2021-10-29 16:02:24 +0300

    init commit

---
+++ app/src/main/java/com/vsu/framespark/android/Grid.kt
@@ -0,0 +1,11 @@
+package com.vsu.framespark.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Grid : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_grid)
+    }
+}
